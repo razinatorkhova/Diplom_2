196eed67bcb6c6a9ee06a9b8913f78fe0432a5cc
Razina Torkhova <razina.torkhova@gmail.com>
2025-03-10 08:50:27 +0100

    add corrected tests classes

---
+++ src/test/java/tests/apiauthlogin/LoginUserTest.java
@@ -0,0 +1,65 @@
+package tests.apiauthlogin;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.practikum.yandex.api.UserApi;
+import ru.practikum.yandex.model.lombok.LoginDataLombok;
+import ru.practikum.yandex.model.lombok.UserDataLombok;
+
+import static ru.practikum.yandex.model.generator.UserGenerator.getRandomUser;
+
+@Feature("Login user")
+public class LoginUserTest {
+
+    protected String userAccessToken;
+    protected String userRefreshToken;
+    UserApi userApi = new UserApi();
+    UserDataLombok userData;
+
+    @Before
+    public void setUp() {
+        userData = getRandomUser("Vlad54321", "password54321", "Vlad");
+        userApi.createUserLombok(userData);
+    }
+
+    @After
+    public void cleanUp() {
+        if (userRefreshToken != null) {
+            userApi.logoutUser(userRefreshToken);
+        }
+        if (userAccessToken != null) {
+            userApi.deleteUser(userAccessToken);
+        }
+    }
+
+    @DisplayName("Check user can be authorized")
+    @Description("This test verifies that a user can successfully log in using the correct email and password.")
+    @Test
+    public void userCanBeAuthorizedTest() {
+        // Авторизация с использованием email и пароля из созданного пользователя
+        LoginDataLombok loginDataLombok = new LoginDataLombok(userData.getEmail(), userData.getPassword());
+        userApi.loginUser(loginDataLombok);
+    }
+
+    @DisplayName("Check cannot Authorized user with incorrect Email")
+    @Description("This test verifies that a user cannot log in when an incorrect email is provided.")
+    @Test
+    public void cannotAuthorizedUserWithIncorrectEmailTest() {
+
+        LoginDataLombok loginDataLombok = new LoginDataLombok("Vlad000001", userData.getPassword());
+        userApi.cannotAuthorizedUserWithIncorrectRequiredField(loginDataLombok);
+    }
+
+    @DisplayName("Check cannot Authorized user with incorrect Password")
+    @Description("This test verifies that a user cannot log in when an incorrect password is provided.")
+    @Test
+    public void cannotAuthorizedUserWithIncorrectPasswordTest() {
+
+        LoginDataLombok loginDataLombok = new LoginDataLombok(userData.getEmail(), "password00001");
+        userApi.cannotAuthorizedUserWithIncorrectRequiredField(loginDataLombok);
+    }
+}
