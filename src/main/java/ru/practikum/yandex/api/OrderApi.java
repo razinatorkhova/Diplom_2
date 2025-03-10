eddae788de542b7d975741a1b683e62d30d57842
Razina Torkhova <razina.torkhova@gmail.com>
2025-03-10 08:45:59 +0100

    add corrected API classes

--- src/main/java/ru/practikum/yandex/api/OrderApi.java
+++ src/main/java/ru/practikum/yandex/api/OrderApi.java
@@ -2,41 +2,89 @@ package ru.practikum.yandex.api;
 
 import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
-import ru.practikum.yandex.model.lombok.OrderDataLombok;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 
 
 public class OrderApi extends RestApi {
-    /**
-     * URI для создания заказа
-     */
-    public static final String CREATE_ORDER_URI = "/api/orders";
-    /**
-     * URI для получения данных об ингредиентах
-     */
-    public static final String GET_DATA_INGREDIENTS_URI = "/api/ingredients";
 
     @Step("Create order")
-    public ValidatableResponse createOrderLombok(OrderDataLombok order) {
+    public ValidatableResponse createOrderLombok(String userAccessToken, Object orderBody) {
         return given()
                 .spec(requestSpecification())
-                .and()
-                .body(order)
+                .header("Authorization", userAccessToken)
+                .body(orderBody)
                 .when()
-                .post(CREATE_ORDER_URI)
-                .then();
+                .post(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("success", is(true));
+    }
+
+    @Step("Create order with ingredients without authorization")
+    public ValidatableResponse createOrderWithoutAuthorizationWithIngredients(String userAccessToken, Object orderBody) {
+        return given()
+                .spec(requestSpecification())
+                .header("Authorization", userAccessToken)
+                .body(orderBody)
+                .when()
+                .post(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is("You should be authorised"));
+    }
+
+    @Step("Create order with authorization without ingredients")
+    public ValidatableResponse createOrderWithAuthorizationWithoutIngredients(String userAccessToken, Object orderBody) {
+        return given()
+                .spec(requestSpecification())
+                .header("Authorization", userAccessToken)
+                .body(orderBody)
+                .when()
+                .post(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("success", is(false))
+                .body("message", is("Ingredient ids must be provided"));
+    }
+
+    @Step("Create order with authorization with invalid ingredients")
+    public ValidatableResponse createOrderWithAuthorizationWithInvalidIngredients(String userAccessToken, Object orderBody) {
+        return given()
+                .spec(requestSpecification())
+                .header("Authorization", userAccessToken)
+                .body(orderBody)
+                .when()
+                .post(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 
     @Step("Get ingredients")
-    public ValidatableResponse getIngredients(List<String> ingredients) {
-        return given()
+    public List<String> getIngredients() {
+        ValidatableResponse response = given()
                 .spec(requestSpecification())
                 .when()
-                .get(GET_DATA_INGREDIENTS_URI)
-                .then();
+                .get(Endpoints.GET_DATA_INGREDIENTS_URI)
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+
+        return response.extract().jsonPath().getList("data._id");
     }
 
     @Step("Get list of orders from specific user")
@@ -46,7 +94,28 @@ public class OrderApi extends RestApi {
                 .header("Authorization", userAccessToken)
                 .and()
                 .when()
-                .get(CREATE_ORDER_URI)
-                .then();
+                .get(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("success", is(true))
+                .body("orders", is(not(empty())));
+    }
+
+    @Step("Get list of orders from specific user without authorization")
+    public ValidatableResponse getListOrdersLombokWithoutAuthorization(String userAccessToken) {
+        return given()
+                .spec(requestSpecification())
+                .header("Authorization", userAccessToken)
+                .and()
+                .when()
+                .get(Endpoints.CREATE_ORDER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is("You should be authorised"));
     }
 }
--- src/main/java/ru/practikum/yandex/api/UserApi.java
+++ src/main/java/ru/practikum/yandex/api/UserApi.java
@@ -2,52 +2,157 @@ package ru.practikum.yandex.api;
 
 import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
 import ru.practikum.yandex.model.lombok.LoginDataLombok;
 import ru.practikum.yandex.model.lombok.UserDataLombok;
 import ru.practikum.yandex.model.lombok.UserDataUpdateLombok;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class UserApi extends RestApi {
-    /**
-     * URI для создания пользователя
-     */
-    public static final String CREATE_USER_URI = "/api/auth/register";
-    /**
-     * URI для авторизации пользователя
-     */
-    public static final String LOGIN_USER_URI = "/api/auth/login";
-    /**
-     * URI для получения, обновления и удаления пользователя
-     */
-    public static final String GET_CHANGE_DELETE_DATA_USER_URI = "/api/auth/user";
-    /**
-     * URI для выхода из системы
-     */
-    public static final String LOGOUT_USER_URI = "/api/auth/logout";
-
 
     @Step("Create user")
-    public ValidatableResponse createUserLombok(UserDataLombok user) {
+    public ValidatableResponse createUserLombok(UserDataLombok userData) {
         return given()
                 .spec(requestSpecification())
                 .and()
-                .body(user)
+                .body(userData)
                 .when()
-                .post(CREATE_USER_URI)
-                .then();
+                .post(Endpoints.CREATE_USER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("success", is(true));
     }
 
-    @Step("Updated data user")
-    public ValidatableResponse updatedDataUser(UserDataUpdateLombok user, String userAccessToken) {
+    @Step("Check can not create two same users")
+    public ValidatableResponse cannotCreateTwoSameUsers(UserDataLombok userData) {
         return given()
                 .spec(requestSpecification())
-                .header("Authorization", userAccessToken)
+                .and()
+                .body(userData)
+                .when()
+                .post(Endpoints.CREATE_USER_URI)
+                .then().log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .body("success", is(false))
+                .body("message", is("User already exists"));
+    }
+
+    @Step("Check cannot create user without required field")
+    public ValidatableResponse cannotCreateUserWithoutRequiredField(UserDataLombok userData) {
+        return given()
+                .spec(requestSpecification())
+                .and()
+                .body(userData)
+                .when()
+                .post(Endpoints.CREATE_USER_URI)
+                .then().log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .body("success", is(false))
+                .body("message", is("Email, password and name are required fields"));
+    }
+
+    @Step("Authorized user")
+    public ValidatableResponse loginUser(LoginDataLombok user) {
+        return given()
+                .spec(requestSpecification())
                 .and()
                 .body(user)
                 .when()
-                .patch(GET_CHANGE_DELETE_DATA_USER_URI)
-                .then();
+                .post(Endpoints.LOGIN_USER_URI)
+                .then().log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("success", is(true));
+    }
+
+    @Step("Check login and get accessToken response")
+    public String loginAndGetAccessToken(LoginDataLombok loginData) {
+        ValidatableResponse loginResponse = loginUser(loginData);
+        return loginResponse.extract().path("accessToken");
+    }
+
+    @Step("Check cannot Authorized user with incorrect required field")
+    public ValidatableResponse cannotAuthorizedUserWithIncorrectRequiredField(LoginDataLombok user) {
+        return given()
+                .spec(requestSpecification())
+                .and()
+                .body(user)
+                .when()
+                .post(Endpoints.LOGIN_USER_URI)
+                .then().log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is("email or password are incorrect"));
+    }
+
+    @Step("Check can update name with authorization")
+    public ValidatableResponse canUpdateNameWithAuthorization(UserDataUpdateLombok userDataUpdate, String userAccessToken, String expectedName) {
+        RequestSpecification request = given()
+                .spec(requestSpecification())
+                .body(userDataUpdate);
+
+        // Добавляем заголовок только если userAccessToken не null
+        if (userAccessToken != null) {
+            request.header("Authorization", userAccessToken);
+        }
+
+        return request
+                .when()
+                .patch(Endpoints.GET_CHANGE_DELETE_DATA_USER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("user.name", is(expectedName));
+    }
+
+    @Step("Check can update email with authorization")
+    public ValidatableResponse canUpdateEmailWithAuthorization(UserDataUpdateLombok userDataUpdate, String userAccessToken, String expectedEmail) {
+        RequestSpecification request = given()
+                .spec(requestSpecification())
+                .body(userDataUpdate);
+
+        // Добавляем заголовок только если userAccessToken не null
+        if (userAccessToken != null) {
+            request.header("Authorization", userAccessToken);
+        }
+
+        return request
+                .when()
+                .patch(Endpoints.GET_CHANGE_DELETE_DATA_USER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("user.email", is(expectedEmail.toLowerCase()));
+    }
+
+    @Step("Check unauthorized update data response")
+    public ValidatableResponse cannotUpdateDataWithoutAuthorization(UserDataUpdateLombok user, String userAccessToken) {
+        RequestSpecification request = given()
+                .spec(requestSpecification())
+                .body(user);
+        // Добавляем заголовок только если userAccessToken не null
+        if (userAccessToken != null) {
+            request.header("Authorization", userAccessToken);
+        }
+        return request
+                .when()
+                .patch(Endpoints.GET_CHANGE_DELETE_DATA_USER_URI)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("success", is(false))
+                .body("message", is("You should be authorised"));
     }
 
     @Step("Delete user")
@@ -56,53 +161,20 @@ public class UserApi extends RestApi {
                 .spec(requestSpecification())
                 .header("Authorization", userAccessToken) // Добавляем токен в заголовки
                 .when()
-                .delete(GET_CHANGE_DELETE_DATA_USER_URI)
+                .delete(Endpoints.GET_CHANGE_DELETE_DATA_USER_URI)
                 .then();
     }
 
-        @Step("Authorized user")
-    public ValidatableResponse loginUser(LoginDataLombok user) {
+    @Step("Logout user")
+    public ValidatableResponse logoutUser(String userRefreshToken) {
+        // Создание JSON тела запроса
+        String requestBody = String.format("{\"token\": \"%s\"}", userRefreshToken);
+
         return given()
-                .spec(requestSpecification())
-                .and()
-                .body(user)
+                .header("Content-Type", "application/json") // Указание заголовка, если это необходимо
+                .body(requestBody)
                 .when()
-                .post(LOGIN_USER_URI)
-                .then();
-    }
-
-
-@Step("Logout user")
-public ValidatableResponse logoutUser(String userRefreshToken) {
-    // Создание JSON тела запроса
-    String requestBody = String.format("{\"token\": \"%s\"}", userRefreshToken);
-
-    return given()
-            .header("Content-Type", "application/json") // Указание заголовка, если это необходимо
-            .body(requestBody)
-            .when()
-            .post(LOGOUT_USER_URI)
-            .then();
-}
-    @Step("Change data user")
-    public ValidatableResponse changeUserData(String userAccessToken, UserDataUpdateLombok updatedUserData) {
-        return given()
-                .spec(requestSpecification())
-                .header("Authorization", userAccessToken) // Добавляем токен в заголовки
-                .body(updatedUserData) // Передаем обновленные данные в теле запроса
-                .when()
-                .patch(GET_CHANGE_DELETE_DATA_USER_URI)
-                .then();
-    }
-
-
-    @Step("Get user data")
-    public ValidatableResponse getUserData(String userAccessToken) {
-        return given()
-                .spec(requestSpecification())
-                .header("Authorization", userAccessToken)
-                .when()
-                .get(GET_CHANGE_DELETE_DATA_USER_URI)
+                .post(Endpoints.LOGOUT_USER_URI)
                 .then();
     }
 }
